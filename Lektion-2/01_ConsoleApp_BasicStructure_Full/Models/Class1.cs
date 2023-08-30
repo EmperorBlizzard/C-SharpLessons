9c8d07511682f736488a46603f1efa1e22524e29
Emil Olsson <emil111@live.se>
2023-08-30 10:25:48 +0200

    first

---
+++ Lektion-2/01_ConsoleApp_BasicStructure_Full/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ConsoleApp_BasicStructure_Full.Models
+{
+    internal class Class1
+    {
+    }
+}
