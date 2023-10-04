c9e4febc91f0a69ca7495376b6a02646e5f2b28b
Emil Olsson <emil111@live.se>
2023-10-04 14:31:37 +0200

    Lesson 10 content

---
+++ Letion-10/FirstPart/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            if (count == 10)
+                label1.Text = "Wow 10 clicks";
+            else if (count == 100)
+                label1.Text = "OMG 100 clicks your insane";
+            else if (count == 1000)
+                label1.Text = "Go touch grass";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
---
+++ Letion-10/TodoList/TodoListApp/Models/Todo.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoListApp.Models;
+
+class Todo
+{
+    public string Activity { get; set; }
+    public DateTime Created { get; set; }= DateTime.Now;
+}
